f48de2af26160e28e32d0c69fffcacffd70f72db
Jose <josexmoreno1998@gmail.com>
2024-02-28 17:47:23 -0300

    convert table to grid instead

--- src/Components/OutputInterface.tsx
+++ src/Components/OutputInterface.tsx
@@ -7,46 +7,31 @@ const outputState = state.slice(0, state.length - 1);
 const [menuOpenAt, setMenuOpenAt] = useState(-1);
 
 return (
-      <table className="output-section flex-center">
-	<thead>
-	  <tr>
-	    <th className="qty">Uds.</th>
-	    <th className="client">Paciente</th>
-	    <th className="product">Producto</th> <th className="price">Precio</th>
-	    <th className="delete">Delete</th>
-	  </tr>
-	</thead>
-	<tbody>
-	{outputState.map((currentValue, index) => {
-	  return (
-	      menuOpenAt != index ?
-
-	      <tr key={index} onClick={()=> setMenuOpenAt(index)} >
-		<td className="qty" >{currentValue.productQuantity}</td>
-		<td className="client">{currentValue.clientName}</td>
-		<td className="product">{currentValue.productName}</td>
-		<td className="price">{formatToCurrency(currentValue.totalPrice)}</td>
-		<td className="delete"><button onClick={()=> handleChange(undefined, index, 'delete')}>Delete</button></td>
-	      </tr>
-
+      <div className="output-section ">
+	  <ul className="table-head grid-table">
+	    <li >Uds.</li>
+	    <li >Paciente</li>
+	    <li >Producto</li>
+	    <li >Precio</li>
+	    <li >Delete</li>
+	  </ul>
+	  <ul>
+	    {outputState.map((element, index)=> {
+	      return (
+	      menuOpenAt != index ? 
+	      <li className="grid-table body-table" onClick={()=> setMenuOpenAt(index)}>
+		<span>{element.productQuantity}</span>
+		<span className="bold">{element.clientName}</span>
+		<span>{element.productName}</span>
+		<span className="bold">$ {formatToCurrency(element.totalPrice)}</span>
+		<button onClick={() => handleChange(undefined, index, 'delete')}>Delete</button>
+	      </li>
 	      : 
-
-	      <tr className="table-delete-menu" onClick={()=> setMenuOpenAt(-1)}>
-	      <td></td>
-	      <td colSpan={2} className="menu-delete-header">¿Deseas eliminar este cargo?</td>
-	      <td><button onClick={()=> handleChange(undefined, index, 'delete')}>Si</button></td>
-	      </tr>
-	  )
-	})}
-	</tbody>
-	<tfoot>
-	<tr>
-	<th></th>
-	<th></th>
-	<td><b>Total:$</b>{formatToCurrency(totalPayment)}</td>
-	</tr>
-	</tfoot>
-      </table>
+	      <li className="body-table delete-menu" onClick={() => setMenuOpenAt(-1)}><span>¿Deseas eleminar este cargo?</span><button className="delete-button">Si</button></li>
+	      )
+	    })}
+	  </ul>
+      </div>
       )
 }
 export default OutputInterface;
