f245f0949903e0f07f9b60474a04e86a74792012
Jose <josexmoreno1998@gmail.com>
2024-03-02 22:58:55 -0300

    add responsiveness to the about-page and logic for the button delete charge interaction

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-import { ReactDOM } from "react";
-
 function AboutPage() {
   return (
     <section className="about-container">
       <span>Creado Por</span>
       <h1>Jose Moreno</h1>
-      <p>Lorem ipsum dolor sit amet consectetur. Suspendisse adipiscing nulla vitae volutpat. Platea varius neque phasellus ultricies nunc eget. Et proin venenatis ut ultrices scelerisque est integer. Montes lacinia ultrices egestas sit tincidunt varius suspendisse volutpat. Lobortis malesuada diam augue ornare duis quam amet. Turpis id quam neque odio pulvinar porttitor lorem justo non. </p>
+      <p>Lorem ipsum dolor sit amet consectetur. Suspendisse adipiscing nulla vitae volutpat. Platea varius neque phasellus ultricies nunc eget. Et proin venenatis ut ultrices scelerisque est integer.</p>
+      <p className="last-paragraph">Montes lacinia ultrices egestas sit tincidunt varius suspendisse volutpat. Lobortis malesuada diam augue ornare duis quam amet. Turpis id quam neque odio pulvinar porttitor lorem justo non. </p>
       <div className="btn-container">
 	<a href="#" className="portfolio-link">Portafolio Web</a>
 	<a href="#">josexmoreno1998@gmail.com</a>
--- src/Components/OutputInterface.tsx
+++ src/Components/OutputInterface.tsx
@@ -1,36 +1,38 @@
 import { OutputInterfaceProps } from "../interfaces/interfaces";
 import { formatToCurrency } from "../CustomHooks/reducerMethods";
 import { useState } from "react";
+import DeleteSVG from "../assets/delete.svg";
 
 function OutputInterface({state, totalPayment, handleChange}:OutputInterfaceProps){
 const outputState = state.slice(0, state.length - 1);
 const [menuOpenAt, setMenuOpenAt] = useState(-1);
-
 return (
       <div className="output-section ">
 	  <ul className="table-head grid-table">
-	    <li >Uds.</li>
-	    <li className="client-column">Paciente</li>
+	    <li>Uds.</li>
+	    <li >Paciente</li>
 	    <li >Producto</li>
-	    <li >Precio</li>
-	    <li className="desktop-delete-btn">Delete</li>
+	    <li>Precio</li>
+	    <li className="delete-column">Delete</li>
 	  </ul>
 	  <ul>
 	    {outputState.map((element, index)=> {
 	      return (
-	      menuOpenAt != index ? 
-	      <li className="grid-table body-table" onClick={()=> setMenuOpenAt(index)}>
-		<span>{element.productQuantity}</span>
-		<span className="bold">{element.clientName}</span>
-		<span>{element.productName}</span>
-		<span className="bold">$ {formatToCurrency(element.totalPrice)}</span>
-		<button className="desktop-delete-btn" onClick={() => handleChange(undefined, index, 'delete')}>Delete</button>
-	      </li>
+	      menuOpenAt == index ? 
+	      <li className="body-table delete-menu" onClick={() => setMenuOpenAt(-1)}><span>¿Deseas eleminar este cargo?</span><button onClick={()=>handleChange(undefined, index, 'delete')} className="mobile-delete-button">Si</button></li>
 	      : 
-	      <li className="body-table delete-menu" onClick={() => setMenuOpenAt(-1)}><span>¿Deseas eleminar este cargo?</span><button onClick={()=>handleChange(undefined, index, 'delete')} className="delete-button">Si</button></li>
+	      <li className="grid-table body-table" onClick={()=> window.innerWidth < 800 ? setMenuOpenAt(index) : null}>
+		<span>{element.productQuantity}</span>
+		<span className="wrap-text">{element.clientName}</span>
+		<span className="wrap-text">{element.productName}</span>
+		<span>$&nbsp;&nbsp;{formatToCurrency(element.totalPrice)}</span>
+		<button className="desktop-delete-btn" onClick={() => handleChange(undefined, index, 'delete')}>
+		  <img src={DeleteSVG} width={30} height={30} alt="delete charge"/>
+		</button>
+	      </li>
 	      )
 	    })}
-	    <li className="total-payment-row"><span className="bold">TOTAL:&nbsp;</span>${formatToCurrency(totalPayment)}</li>
+	    <li className="total-payment-row"><span className="bold">TOTAL:&nbsp;&nbsp;</span>${formatToCurrency(totalPayment)}</li>
 	  </ul>
 
       </div>
